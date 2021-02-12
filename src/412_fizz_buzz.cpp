20ceeb4e883a4b056eb8ae55911f02c1608e15d6
hariharanragothaman <hariharanragothaman@gmail.com>
2021-02-12 14:25:36 -0500

    Adding basic fizzbuzz logic

--- src/412_fizz_buzz.cpp
+++ src/412_fizz_buzz.cpp
@@ -32,10 +32,34 @@ Return:
 vector<string> fizzBuzz(int n)
 {
     vector<string> result = {};
+
+    for(int i = 1; i <= n; i++)
+    {
+        if (i % 3 == 0 and i % 5 == 0)
+        {
+            result.push_back("FizzBuzz");
+        }
+        else if (i % 5 == 0)
+        {
+            result.push_back("Buzz");
+        }
+        else if(i % 3 == 0)
+        {
+            result.push_back("Fizz");
+        }
+        else
+        {
+            result.push_back(to_string(i));
+        }
+    }
     return result;
 }
 
 int main()
 {
+    vector<string> result = fizzBuzz(15);
+    for(auto&it : result)
+        cout << it << endl;
     return 0;
+
 }
